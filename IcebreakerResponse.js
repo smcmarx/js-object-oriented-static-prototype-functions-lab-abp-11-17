ec1f38c16fa6e1b1013745282661aa9d1f9eff5f
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-15 15:36:55 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emails){
+    return emails.map(function(email){
+      return new IcebreakerResponse(email)
+    })
+}
