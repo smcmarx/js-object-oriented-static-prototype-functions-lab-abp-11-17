289110354a8f02bf8628e4e12d25abfac6d9838e
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-15 15:36:07 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -4,6 +4,6 @@ function IcebreakerResponse(email){
 
 IcebreakerResponse.BatchCreate = function(emails){
     return emails.map(function(email){
-      new IcebreakersResponse(email)
+      new IcebreakerResponse(email)
     })
 }
